e859e91c3c808e32a0f08995c11369a97fcd82c5
rafaeldejesusl <rafaeldejesuspp@gmail.com>
2023-02-15 17:33:20 -0300

    :wrench: chore: Criando as migrations de clientes

--- cadastro/admin.py
+++ cadastro/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from cadastro.models import Cliente
 
-# Register your models here.
+class Clientes(admin.ModelAdmin):
+  list_display = ('id', 'nome', 'cpf')
+  list_display_links = ('id', 'nome')
+  search_fields = ('nome',)
+
+admin.site.register(Cliente, Clientes)
--- cadastro/models.py
+++ cadastro/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Cliente(models.Model):
+  nome = models.CharField(max_length=40)
+  cpf = models.CharField(max_length=11)
+
+  def __str__(self) -> str:
+    return self.nome
