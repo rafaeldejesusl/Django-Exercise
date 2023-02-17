b29a6bc03cd768e107ba8199507ac0a8cc17ff1c
rafaeldejesusl <rafaeldejesuspp@gmail.com>
2023-02-17 17:15:52 -0300

    :sparkles: feat: Adicionando a url da api

--- config/urls.py
+++ config/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from cadastro.views import ClientesViewSet
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register(r'clientes', ClientesViewSet)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", include(router.urls)),
 ]
