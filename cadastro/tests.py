0f39ede9905854a910ac2cbca2bf0fc0da96035e
rafaeldejesusl <rafaeldejesuspp@gmail.com>
2023-02-17 18:34:41 -0300

    :test_tube: test: Adicionando teste de listagem de clientes

--- cadastro/tests.py
+++ cadastro/tests.py
@@ -2,7 +2,7 @@ from rest_framework import status
 from rest_framework.test import APITestCase
 from cadastro.models import Cliente
 
-clienteMock = { 'nome': 'Teste', 'cpf': '00000000000' }
+clienteMock = { 'id': 1,'nome': 'Teste', 'cpf': '00000000000' }
 
 class CadastroTests(APITestCase):
   def test_create_client(self):
@@ -11,3 +11,9 @@ class CadastroTests(APITestCase):
     self.assertEqual(Cliente.objects.count(), 1)
     self.assertEqual(Cliente.objects.get().nome, clienteMock['nome'])
     self.assertEqual(Cliente.objects.get().cpf, clienteMock['cpf'])
+  
+  def test_get_client(self):
+    self.client.post('/clientes/', clienteMock, format='json')
+    response = self.client.get('/clientes/')
+    self.assertEqual(response.status_code, status.HTTP_200_OK)
+    self.assertEqual(response.data, [clienteMock])
