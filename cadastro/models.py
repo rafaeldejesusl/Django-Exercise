a3180590c9fc4c9be9a1051007c264d8c18d7930
rafaeldejesusl <rafaeldejesuspp@gmail.com>
2023-02-24 16:43:45 -0300

    :sparkles: feat: Adicionando data de nascimento na model

--- cadastro/models.py
+++ cadastro/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from datetime import date
 
 
 class Cliente(models.Model):
     nome = models.CharField(max_length=40)
     cpf = models.CharField(max_length=11)
+    data_de_nascimento = models.DateField(default=date.min)
 
     def __str__(self) -> str:
         return self.nome
