1c56d9c7f034acc6ceee06bebfd4df7b7620c7a6
rafaeldejesusl <rafaeldejesuspp@gmail.com>
2023-02-24 17:09:07 -0300

    :sparkles: feat: Adicionando email na model

--- cadastro/models.py
+++ cadastro/models.py
@@ -4,8 +4,13 @@ from datetime import date
 
 class Cliente(models.Model):
     nome = models.CharField(max_length=40)
-    cpf = models.CharField(max_length=11)
+    cpf = models.CharField(max_length=11, unique=True)
     data_de_nascimento = models.DateField(default=date.min)
+    email = models.EmailField(
+        max_length=40,
+        default="example@email.com",
+        unique=True
+    )
 
     def __str__(self) -> str:
         return self.nome
